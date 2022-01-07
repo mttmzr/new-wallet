e8df9dd7bf1fe44c3e08969110f9d6ba68f3e472
mttmzr <mttmzr@users.noreply.github.com>
2022-01-07 16:08:07 +0100

    Add commander and add mnemonic option

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 
 const { Wallet } = require('ethers')
+const { program } = require('commander')
 
-const wallet = Wallet.createRandom()
+program.version(require('./package.json').version)
+
+program.option('-m, --mnemonic <mnemonic>', 'Mnemonic phrase to use instead of randomly generated one')
+
+program.parse(process.argv)
+
+const mnemonic = program.opts().mnemonic
+const wallet = mnemonic ? Wallet.fromMnemonic(mnemonic) : Wallet.createRandom()
 
 console.log(`Wallet address: ${wallet.address}\n`)
 console.log(`Save the following two, but keep'em super private!`)
