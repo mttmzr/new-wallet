398008d54c6851e2eeb84b9350ffd442a76a099b
mttmzr <mttmzr@users.noreply.github.com>
2022-01-07 16:17:16 +0100

    Add -i option

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const { program } = require('commander')
 program.version(require('./package.json').version)
 
 program.option('-m, --mnemonic <mnemonic>', 'Mnemonic phrase to use instead of randomly generated one')
+program.option('-i, --index <index>', 'Index to be used with derivation path', '0')
 
 program.parse(process.argv)
 
-const mnemonic = program.opts().mnemonic
-const wallet = mnemonic ? Wallet.fromMnemonic(mnemonic) : Wallet.createRandom()
+const { index, mnemonic } = program.opts()
+const path = `m/44'/60'/0'/0/${index}`
+const wallet = mnemonic ? Wallet.fromMnemonic(mnemonic, path) : Wallet.createRandom({ path })
 
 console.log(`Wallet address: ${wallet.address}\n`)
 console.log(`Save the following two, but keep'em super private!`)
